6912acbcd3287e8152805d4b155b6a388f21096e
see7788 <kitchens@qq.com>
2026-06-20 04:13:20 +0800

    release: snapshot source workspace

--- honoapp/src/index.ts
+++ honoapp/src/index.ts
@@ -2,5 +2,5 @@
 
 import runtime from "./runtime";
 import routers from "./routers";
-import store from "./store" 
-runtime.init(routers).then(()=>store.getState().tplActions.codexTplMaterialize());
+import store from "./store";
+runtime.init(routers).then(() => store.getState().tplActions.codexTplMaterialize());
